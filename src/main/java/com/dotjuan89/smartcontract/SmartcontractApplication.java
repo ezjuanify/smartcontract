d920d0a59480ff7a6e682a9887e4050a26a72a3f
dotjuan89 <dotjuan89@gmail.com>
2022-11-16 21:03:38 +0800

    Initial commit

---
+++ src/main/java/com/dotjuan89/smartcontract/SmartcontractApplication.java
@@ -0,0 +1,13 @@
+package com.dotjuan89.smartcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcontractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcontractApplication.class, args);
+	}
+
+}
